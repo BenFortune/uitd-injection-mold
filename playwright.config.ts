13bac4c760ce166f883c3774b8bb384ab37235cc
Ben Fortune <ben.fortune@gmail.com>
2023-07-21 20:24:53 -0500

    Update configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,6 +21,13 @@ const config: PlaywrightTestConfig = {
   },
   use: {
     baseURL: 'http://localhost:3000',
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
   },
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
@@ -42,16 +49,6 @@ const config: PlaywrightTestConfig = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
   /* Configure projects for major browsers */
   projects: [
     {
