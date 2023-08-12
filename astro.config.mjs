beeda57c2b85e529cca31ee77c7c0c2b8e27fc95
Ben Fortune <ben.fortune@gmail.com>
2023-08-11 20:44:28 -0500

    Updates for adjusting sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: vercel({
-        analytics: true
-    }),
+  adapter: vercel({
+    analytics: true
+  }),
+  integrations: [sitemap()],
+  site: 'https://upperiowatoolanddieinjectionmoldingandtooling.com'
 });
